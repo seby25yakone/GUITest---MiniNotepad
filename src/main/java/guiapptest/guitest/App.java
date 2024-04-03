0ff3ac4b3e9d8dcf1277858f6d677c6ba07d5181
Trasca Sebastian <sebastiangeoioge@gmail.com>
2024-04-03 17:29:27 +0300

    removed obsolete scroll pane and added save file functionality

--- src/main/java/guiapptest/guitest/App.java
+++ src/main/java/guiapptest/guitest/App.java
@@ -2,6 +2,7 @@ package guiapptest.guitest;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,8 +11,8 @@ import java.io.IOException;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("scene1.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        Parent root=FXMLLoader.load(App.class.getResource("scene1.fxml"));
+        Scene scene = new Scene(root);
         stage.setTitle("Yakone's application");
         stage.setScene(scene);
         stage.show();
--- src/main/java/guiapptest/guitest/Scene1Controller.java
+++ src/main/java/guiapptest/guitest/Scene1Controller.java
@@ -2,7 +2,44 @@ package guiapptest.guitest;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Scene1Controller {
+    @FXML
+    private TextArea textArea;
 
+    @FXML
+    public void fileWrite() {
+        // Create a FileChooser
+        FileChooser fileChooser = new FileChooser();
+
+        // Set initial directory (optional)
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+
+        // Set extension filter (optional)
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        // Show save dialog
+        File file = fileChooser.showSaveDialog(textArea.getScene().getWindow());
+
+        // Proceed if a file is selected
+        if (file != null) {
+            try {
+                BufferedWriter bf = new BufferedWriter(new FileWriter(file));
+                bf.write(textArea.getText());
+                bf.flush();
+                bf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
