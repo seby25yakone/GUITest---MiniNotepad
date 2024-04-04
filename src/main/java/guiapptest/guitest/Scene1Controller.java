590e7c76d8c5ee6c500c60342f063ab1f73c7e0a
Trasca Sebastian <sebastiangeoioge@gmail.com>
2024-04-04 17:00:17 +0300

    added colorpicker for font color

--- src/main/java/guiapptest/guitest/Scene1Controller.java
+++ src/main/java/guiapptest/guitest/Scene1Controller.java
@@ -6,6 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
@@ -94,4 +95,14 @@ public class Scene1Controller {
         wordCount.setText("Word count: " + words.countTokens());
     }
 
+    public void setTextColor(){
+        textArea.setStyle("-fx-text-fill:" + toRGBCode(colorPicker.getValue()) +";");
+    }
+
+    private String toRGBCode(Color color) {
+        return String.format("#%02X%02X%02X",
+                (int) (color.getRed() * 255),
+                (int) (color.getGreen() * 255),
+                (int) (color.getBlue() * 255));
+    }
 }
